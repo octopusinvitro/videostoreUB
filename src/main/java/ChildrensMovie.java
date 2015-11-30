02f83a651ec67587249723be23f2766de7befba3
private <user@example.com>
2015-11-30 09:41:52 +0000

    Add RegularMovie class.

--- src/main/java/ChildrensMovie.java
+++ src/main/java/ChildrensMovie.java
@@ -1,5 +1,5 @@
 public class ChildrensMovie extends Movie {
-    
+
     public ChildrensMovie(String name, int priceCode) {
         super(name, priceCode);
     }
---
+++ src/main/java/RegularMovie.java
@@ -0,0 +1,6 @@
+public class RegularMovie extends Movie {
+    
+    public RegularMovie(String name, int priceCode) {
+        super(name, priceCode);
+    }
+}
