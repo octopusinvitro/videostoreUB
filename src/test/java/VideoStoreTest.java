dbc246a270cd297019ea1a91a6a149bea8edb2f7
private <user@example.com>
2015-11-30 08:06:25 +0000

    Change tests to use JUnit4.

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -1,30 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
 
-public class VideoStoreTest extends TestCase {
-    public VideoStoreTest(String name) {
-        super(name);
-    }
+import static org.junit.Assert.assertEquals;
 
-    protected void setUp() {
+public class VideoStoreTest {
+
+    @Before
+    public void setUp() {
         customer = new Customer("Fred");
     }
 
-    public void testSingleNewReleaseStatement() {
+    @Test
+    public void singleNewReleaseStatement() {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement());
     }
 
-    public void testDualNewReleaseStatement() {
+    @Test
+    public void dualNewReleaseStatement() {
         customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
         assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement());
     }
 
-    public void testSingleChildrensStatement() {
+    @Test
+    public void singleChildrensStatement() {
         customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
         assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement());
     }
 
-    public void testMultipleRegularStatement() {
+    @Test
+    public void multipleRegularStatement() {
         customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
         customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
         customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
